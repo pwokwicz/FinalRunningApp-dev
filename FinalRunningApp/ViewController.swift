3ad0ddc3236d2545fc1fa8595ff708d00af95bd1
Parker Wokwicz <pwokwicz@parkers-macbook-air.local>
2019-04-17 11:26:58 -0500

    Initial Commit

---
+++ FinalRunningApp/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  FinalRunningApp
+//
+//  Created by Ashwin Kasargode on 4/17/19.
+//  Copyright © 2019 Ashwin Kasargode. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    var urmom = String()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
